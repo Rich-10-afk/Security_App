9af62549649a6a3cecd3624be1e6834a81bbe17b
Rich-10-afk <richiemwangi19@gmail.com>
2026-03-16 22:42:00 +0300

    Initial Commit

---
+++ Security_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Security_App
+//
+//  Created by Ryan Kibet on 16/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
